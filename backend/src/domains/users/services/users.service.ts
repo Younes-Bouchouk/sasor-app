377ae680d88b9fc513ef559d5ad61824d5db5ad3
Younès Bouchouk <younes.78.alg@gmail.com>
2025-01-26 01:34:05 +0100

    Configuration de prisma + installation de class-validator et class-transformer pour les dto

--- backend/src/domains/users/services/users.service.ts
+++ backend/src/domains/users/services/users.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { PrismaService } from '../../../../src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
   findOne(id: number) {
