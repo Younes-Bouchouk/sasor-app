c2d80f11f40f839e5c880b65949de9c95b8237ea
H_Khalid__H <khalidjj78200@icloud.com>
2025-02-07 22:30:07 +0100

    bug a regler voir evenement sa fonctionne

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { UserModule } from './domains/user/user.module';
 import { FollowModule } from './domains/follow/follow.module';
 import { AuthModule } from './domains/auth/auth.module';
-
+import {EventModule} from './domains/event/event.module'
 @Module({
     imports: [
         UserModule, 
         FollowModule,
-        AuthModule
+        AuthModule,
+        EventModule
     ],
     controllers: [AppController],
 })
