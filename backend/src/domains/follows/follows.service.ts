d3efabaa68779d23c4b7cf51a459d86fd3970d11
Younès Bouchouk <younes.78.alg@gmail.com>
2025-02-10 12:08:46 +0100

    Commentaire dans le follow finito + Création de quelques fonctions privées pour les requêtes répétitives

--- backend/src/domains/follows/follows.service.ts
+++ backend/src/domains/follows/follows.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateFollowDto } from './dto/create-follow.dto';
 import { UserTokenData } from 'src/types/AuthUser';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { DeleteFollowDto } from './dto/delete-follow.dto';
 
 @Injectable()
 export class FollowsService {
@@ -16,21 +17,21 @@ export class FollowsService {
      */
     async create(createFollowDto: CreateFollowDto, user: UserTokenData) {
         // Vérifier que l'utilisateur qui suit existe
-        const existingFollower = await this.checkUserExist(user.id)
+        const existingFollower = await this.checkUserExist(user.id);
         if (!existingFollower)
             throw new BadRequestException("Le compte du suiveur n'existe pas");
-        
+
         // Vérifier que l'utilisateur à suivre existe
-        const existingFollowing = await this.checkUserExist(createFollowDto.followingId)
+        const existingFollowing = await this.checkUserExist(
+            createFollowDto.followingId,
+        );
         if (!existingFollowing)
             throw new BadRequestException("Le compte à suivre n'existe pas");
 
         // Empêche l'utilisateur de se suivre lui même
         if (existingFollower.id === existingFollowing.id)
-            throw new BadRequestException(
-                'Vous ne pouvez pas suivre votre propre compte',
-            );
-        
+            throw new BadRequestException("Vous ne pouvez pas suivre votre propre compte")
+
         // Vérifier si l'utilisateur est déjà suivit
         const alreadyFollow = await this.prisma.follow.findFirst({
             where: {
@@ -48,62 +49,43 @@ export class FollowsService {
                 followingId: createFollowDto.followingId,
             },
         });
-        return 'L\'utilisateur a été suivie avec succès';
+        return "L'utilisateur a été suivie avec succès";
     }
 
+    /*
+     * Permet de récupérer la liste des comptes qui suivent l'utilisateur connecté
+     */
     findMyFollowers(user: UserTokenData) {
-        return this.prisma.follow.findMany({
-            where: {
-                followingId: user.id,
-            },
-            include: {
-                follower: {
-                    select: { pseudo: true },
-                },
-            },
-        });
+        return this.findFollowersByUserId(user.id);
     }
 
+    /*
+     * Permet de récupérer la liste des comptes que l'utilisateur connecté suit
+     */
     findMyFollowing(user: UserTokenData) {
-        return this.prisma.follow.findMany({
-            where: {
-                followerId: user.id,
-            },
-            include: {
-                following: {
-                    select: { pseudo: true },
-                },
-            },
-        });
+        return this.findFollowingByUserId(user.id);
     }
 
-    findUserFollowers(userId) {
-        return this.prisma.follow.findMany({
-            where: {
-                followingId: userId,
-            },
-            include: {
-                follower: {
-                    select: { pseudo: true },
-                },
-            },
-        });
+    /*
+     * Permet de récupérer la liste des comptes qui suivent un utilisateur
+     */
+    findUserFollowers(userId: number) {
+        return this.findFollowersByUserId(userId);
     }
 
-    findUserFollowing(userId) {
-        return this.prisma.follow.findMany({
-            where: {
-                followerId: userId,
-            },
-            include: {
-                following: {
-                    select: { pseudo: true },
-                },
-            },
-        });
+    /*
+     * Permet de récupérer la liste des comptes qu'un utilisateur suit
+     */
+    findUserFollowing(userId: number) {
+        return this.findFollowingByUserId(userId);
     }
 
-    async remove(deleteFollowDto, user: UserTokenData) {
+    /*
+     * Permet de à un utilisateur de ne plus suivre un autre qu'il suivait
+     * Vérifie d'abord si le suivis existe
+     * Effectue une requête dans la table 'follows' pour supprimer la ligne
+     */
+    async remove(deleteFollowDto: DeleteFollowDto, user: UserTokenData) {
         const Follow = await this.prisma.follow.findFirst({
             where: {
                 followingId: deleteFollowDto.followingId,
@@ -117,17 +99,53 @@ export class FollowsService {
                 id: Follow.id,
             },
         });
-        return 'vous ne suiver plus ce compte';
+        return 'Vous ne suivez plus ce compte';
     }
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
     /* - - - - - - - - - - FONCTIONS PRIVÉES - - - - - - - - - - */
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-    private async checkUserExist (userId: number) {
+    // Vérifie si un utilisateur existe dans la table 'user'
+    private async checkUserExist(userId: number) {
         return await this.prisma.user.findUnique({
             where: { id: userId },
         });
     }
 
+    /*
+     * Permet de récupérer la liste des comptes qu'un utilisateur suit
+     * requête effectué dans la table 'follow'
+     * Retourne uniquement leurs id et leurs pseudos
+     */
+    private async findFollowersByUserId(userId: number) {
+        return this.prisma.follow.findMany({
+            where: {
+                followingId: userId,
+            },
+            include: {
+                follower: {
+                    select: { id: true, pseudo: true },
+                },
+            },
+        });
+    }
+
+    /*
+     * Permet de récupérer la liste des comptes qu'un utilisateur suit
+     * requête effectué dans la table 'follow'
+     * Retourne uniquement leurs id et leurs pseudos
+     */
+    private async findFollowingByUserId(userId: number) {
+        return this.prisma.follow.findMany({
+            where: {
+                followerId: userId,
+            },
+            include: {
+                following: {
+                    select: { id: true, pseudo: true },
+                },
+            },
+        });
+    }
 }
