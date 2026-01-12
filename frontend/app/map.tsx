4bcbc1b97eba7b016382fc1d3f9152c7ae2dbaa4
khaharr <khalidjj78200@icloud.com>
2026-01-12 10:21:56 +0100

    dev map

---
+++ frontend/app/map.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import EventMap from '../components/EventMap';
+
+export default function MapScreen() {
+  return (
+    <View style={styles.container}>
+      <EventMap />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
